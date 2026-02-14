e5d33d16960e518adfc2f51397552a3406198a4e
Szymon Solak <16944591+szymon-solak@users.noreply.github.com>
2026-02-14 18:11:28 +0100

    feat: Containerize app

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,20 +1,36 @@
 import { opentelemetry } from "@elysiajs/opentelemetry";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { SQL } from "bun";
 import { Elysia } from "elysia";
 
+const db = new SQL("postgres://tracing:gnicart@postgres:5432/tracing");
+
 const app = new Elysia()
-	.use(opentelemetry({
-		spanProcessors: [
-			new BatchSpanProcessor(
-				new OTLPTraceExporter({
-					url: 'http://localhost:4318/v1/traces'
-				})
-			)
-		]
-	}))
+	.use(
+		opentelemetry({
+			spanProcessors: [
+				new BatchSpanProcessor(
+					new OTLPTraceExporter({
+						url: "http://tempo:4318/v1/traces",
+					}),
+				),
+			],
+		}),
+	)
 	.get("/", () => "Hello Elysia")
 	.get("/random", () => Math.random())
+	.get("/health", async () => {
+		try {
+			const res = await db`SELECT 1 + 1;`;
+			console.log(res);
+		} catch (error) {
+			console.log(error)
+		}
+
+
+		return { ok: true };
+	})
 	.listen(3007);
 
 console.log(
